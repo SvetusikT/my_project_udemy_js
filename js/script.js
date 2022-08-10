0ab800d664c535b5081e3cb3647b0960ef8a89ab
Sveta <SvetusikT@gmail.com>
2022-08-10 22:00:35 +0300

    script.js editing

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log(1)
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const nameOfFilm = prompt("Один из последних просмотренных фильмов?", "");
+const reviewOfFilm = prompt("На сколько оцените его?", "");
+
+console.log({ nameOfFilm, reviewOfFilm});
