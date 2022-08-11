87a60300a6d94e1e8f9cc302abc8e32f572f508f
Sveta <SvetusikT@gmail.com>
2022-08-11 10:39:18 +0300

    19. Практика, ч.1. Начинаем создавать приложение

--- js/script.js
+++ js/script.js
@@ -7,7 +7,12 @@ const personalMovieDB = {
     privat: false
 };
 
-const nameOfFilm = prompt("Один из последних просмотренных фильмов?", "");
-const reviewOfFilm = prompt("На сколько оцените его?", "");
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = prompt("На сколько оцените его?", "");
 
-console.log({ nameOfFilm, reviewOfFilm});
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
